79472c3e149a4a012e2feea120bb7fc4bfaaeeab
Anthony Agelastos <amagela@sandia.gov>
2026-02-15 18:51:25 -0700

    Update SPARTA build script to have better logging and ignore the log file

--- docs/31_sparta/build-elcapitan.sh
+++ docs/31_sparta/build-elcapitan.sh
@@ -1,37 +1,55 @@
 #!/usr/bin/env bash
 
+# set top-level script parameters
 umask 022
 set -e
 set -x
 
+# create vars for common directories and files
 dir_root="`git rev-parse --show-toplevel`"
-dir_src="${dir_root}/sparta"
-dir_base="` pwd -P `"
+dir_pwd="` pwd -P `"
+dir_src="${dir_pwd}/sparta"
+dir_build="${dir_pwd}/sparta/_build"
+file_log="${dir_pwd}/sparta-build.log"
 
-module list
+# redirect STDOUT and STDERR through tee
+exec &> >(tee >(ts '[%Y-%m-%d %H:%M:%S]' > "${file_log}"))
 
+# let's turn on verbosity now
+set -v
+
+# output for posterity
+hostname
+uptime
+lscpu
+
+# clean and reset source
 pushd "${dir_src}"
 git clean -fdx
 git reset --hard
 popd
-cp -a Makefile.crossroads_serial_spr "${dir_src}/src/MAKE"
-pushd "${dir_src}/src"
-make yes-kokkos
-make -j 16 crossroads_serial_spr
-echo "Resultant build info:"
-ls -lh "`pwd -P`/spa_crossroads_serial_spr"
-cp -a "`pwd -P`/spa_crossroads_serial_spr" "${dir_base}"
+
+# create build directory
+test -d "${dir_build}" && rm -rf "${dir_build}"
+mkdir -p "${dir_build}"
+
+# build
+# list current environment
+module list
+# alter environment
+. ats4_env.sh
+# list current environment
+module list
+pushd "${dir_build}"
+cmake \
+    -C ../cmake/presets/elcapitan_kokkos.cmake \
+    -DPKG_FFT=on \
+    -DBUILD_MPI=on \
+    ../cmake
+/usr/bin/time --verbose -- \
+    nice -n 1 \
+        gmake -j 64
 popd
 
-. ats4_env.sh
-cd sparta
-mkdir _build
-cd _build
-cmake -C ../cmake/presets/elcapitan_kokkos.cmake -DPKG_FFT=on -DBUILD_MPI=on ../cmake
-make -j 64
-flux alloc -N1 --exclusive --setattr=thp=always --setattr=hugepages=512GB -q pbatch
-cd ../examples/cylinder/
-flux run -u --exclusive --verbose -N 1 -n 4 -x -c24 -o cpu-affinity=off -o gpu-affinity=off -o mpibind=on,smt:1,verbose:0  ../../_build/src/spa_elcapitan_kokkos -sf kk -k on g 1 -in in.cylinder
-
-
+# gracefully exit
 exit 0
