dba317db07cdbc88b58f332e108d3af0de7d0c30
Anthony Agelastos <amagela@sandia.gov>
2026-06-10 13:33:35 -0600

    Updated symbolic link script for SPARTA template directory

--- docs/31_sparta/templatedir/sparta_ln.sh
+++ docs/31_sparta/templatedir/sparta_ln.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-set -v
+set -x
 
 test ! -e air.species                    && ln -s ../sparta/examples/cylinder/air.species
 test ! -e air.tce                        && ln -s ../sparta/examples/cylinder/air.tce
 test ! -e air.vss                        && ln -s ../sparta/examples/cylinder/air.vss
 test ! -e circle_R0.5_P10000.surf        && ln -s ../sparta/examples/cylinder/circle_R0.5_P10000.surf
 test ! -e sparta_env_elcapitan.sh        && ln -s ../sparta_env_elcapitan.sh
-test ! -e sparta_fom.py                  && ln -s ../sparta_fom.py
+test ! -e sparta_env_cts2.sh             && ln -s ../sparta_env_cts2.sh
+test ! -e sparta_env_cts2gpu.sh          && ln -s ../sparta_env_cts2gpu.sh
 test ! -e kokkos_tools_env_elcapitan.sh  && ln -s ../kokkos_tools_env_elcapitan.sh
-test ! -e sparta                         && ln -s ../sparta
+test ! -e kokkos_tools_env_cts2gpu.sh    && ln -s ../kokkos_tools_env_cts2gpu.sh
+test ! -e sparta_src                     && ln -s ../sparta
 
 exit 0
