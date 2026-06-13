d30d7e15dfabb3e8dd1ee76c093199015b1b4233
Anthony Agelastos <amagela@sandia.gov>
2026-06-13 10:16:46 -0600

    Initial MaxRSS extraction for SPARTA

--- docs/31_sparta/templatedir/sparta_postprocess.sh
+++ docs/31_sparta/templatedir/sparta_postprocess.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # This scripts extracts useful quantities from the resultant SPARTA
-# output file. A sample excerpt is provided below for reference.
+# output file(s). A sample excerpt is provided below for reference.
 #
+# SAMPLE log.sparta
 #<snip>
 # KOKKOS mode is enabled (/tscratch/amagela/fcr/sparta-final/llnl-benchmarks-sparta/docs/31_sparta/sparta/src/KOKKOS/kokkos.cpp:40)
 #   requested 0 GPU(s) per node
@@ -15,23 +16,36 @@
 #<snip>
 #Performance: 1.481 timesteps/s, 486.612 Mparticle-step/s
 #<snip>
+#
+# SAMPLE output-srun*.log
+#<snip>
+#BEGIN KOKKOS PROFILING REPORT:
+#TOTAL TIME: 671.207 seconds
+#<snip>
+#Host process high water mark memory consumption: 852156 kB
+#
+#END KOKKOS PROFILING REPORT.
 
 
 file_name="log.sparta"
 
-echo "FOM,PPC,NSCALE,ranks,GPUs/node,threads/rank,FILE"
+echo "FOM,PPC,NSCALE,ranks,GPUs/node,threads/rank,MaxRSS (kB),FILE"
 find . \
      -type f \
      -name "${file_name}" \
      -print0 |
 while IFS= read -r -d '' file; do  # <- read each name into $file
+    dir_spartalog="` dirname \"${file}\" `"
     gpus_per_node=`grep -oP 'requested.*?\K\d+(?=\s*GPU\(s\) per node)' "${file}" | sed -n '1{p;q;}'`
     threads_per_rank=`grep -oP 'requested.*?\K\d+(?=\s*thread\(s\) per MPI task)' "${file}" | sed -n '1{p;q;}'`
     ranks=`grep -oP 'Running on .*?\K\d+(?=\s*MPI task\(s\))' "${file}" | sed -n '1{p;q;}'`
     ppc=`grep -oP '^ Particles per cell \(ppc\) = \K[+-]?\d+' "${file}" | sed -n '1{p;q;}'`
     nscale=`grep -oP '^ Weak scaling factor \(nscale\) = \K[+-]?\d+' "${file}" | sed -n '1{p;q;}'`
     fom=`grep -oP '^Performance:.*?[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?.*?\K[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?' "${file}" | sed -n '2{p;q;}'`
-    echo "${fom},${ppc},${nscale},${ranks},${gpus_per_node},${threads_per_rank},${file}"
+
+    maxrss=`grep -oP 'Host process high water mark memory consumption:.*?\K\d+(?=\s*kB)' "${dir_spartalog}"/output-srun*.log | sed -n '1{p;q;}'`
+
+    echo "${fom},${ppc},${nscale},${ranks},${gpus_per_node},${threads_per_rank},${maxrss},${file}"
 done
 
 
