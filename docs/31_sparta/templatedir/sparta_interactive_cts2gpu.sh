aaa40f84ce275f729df9f740d6cea9f0d47005f2
Anthony Agelastos <amagela@sandia.gov>
2026-06-15 15:46:24 -0600

    Added SPARTA interactive job launcher for CTS-2+GPU systems.

---
+++ docs/31_sparta/templatedir/sparta_interactive_cts2gpu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+. sparta_env_cts2gpu.sh
+export EXE_SPARTA="${EXE_SPARTA:-sparta/_build-cts2gpuamber/src/spa_kokkos_cuda}"
+export KOKKOS_TOOLS_LIBS="/tscratch/amagela/fcr/sparta-final/llnl-benchmarks-sparta/docs/31_sparta/kokkos-tools-cts2/profiling/space-time-stack/kp_space_time_stack.so"
+export CUDA_LAUNCH_BLOCKING=1
+mpiexec \
+    -np 4 \
+    "${EXE_SPARTA}" \
+    -k on g 4 -sf kk -in in.cylinder
