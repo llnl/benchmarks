8ff50b1a05d34dd2497a829d2e508639452b61ab
Anthony Agelastos <amagela@sandia.gov>
2026-06-15 15:27:18 -0600

    Added loop around SPARTA bulk launcher to handle single-node weak scaling

--- docs/31_sparta/templatedir/sparta_bulk_launcher.sh
+++ docs/31_sparta/templatedir/sparta_bulk_launcher.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-SPARTA_NSCALE=2 SPARTA_PPC=47 SPARTA_IS_KOKKOS_TOOLS="yes" APP_REPEAT=1 \
-    sbatch \
-        --nodes=2 \
-        --account=fy140252 \
-        --job-name=fcr30sparta \
-        --time=0:59:59 \
-        --partition=batch,short \
-        sparta_sbatch_cts2.sh
+for m_ppc in ` seq 66 125 ` ; do
+    SPARTA_NSCALE=1 SPARTA_PPC=${m_ppc} SPARTA_IS_KOKKOS_TOOLS="yes" APP_REPEAT=1 \
+        sbatch \
+            --nodes=1 \
+            --account=fy140252 \
+            --job-name=fcr30sparta \
+            --time=0:14:59 \
+            --partition=batch,short \
+            sparta_sbatch_cts2.sh
+    sleep 0.5
+done
 
 exit 0
