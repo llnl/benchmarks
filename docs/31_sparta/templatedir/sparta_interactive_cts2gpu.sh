64f6adaa8f42bd4e76d65fe65cde1a2f38f6f9dd
Anthony Agelastos <amagela@sandia.gov>
2026-06-15 17:28:48 -0600

    Modified SPARTA interactive script for sleuthing some CTS-2+GPU results.

--- docs/31_sparta/templatedir/sparta_interactive_cts2gpu.sh
+++ docs/31_sparta/templatedir/sparta_interactive_cts2gpu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 . sparta_env_cts2gpu.sh
-export EXE_SPARTA="${EXE_SPARTA:-sparta/_build-cts2gpuamber/src/spa_kokkos_cuda}"
-export KOKKOS_TOOLS_LIBS="/tscratch/amagela/fcr/sparta-final/llnl-benchmarks-sparta/docs/31_sparta/kokkos-tools-cts2/profiling/space-time-stack/kp_space_time_stack.so"
+export EXE_SPARTA="${EXE_SPARTA:-sparta/_build-cts2gpu/src/spa_kokkos_cuda}"
+export KOKKOS_TOOLS_LIBS="/tscratch/amagela/fcr/sparta-final/llnl-benchmarks-sparta/docs/31_sparta/kokkos-tools-cts2gpu/profiling/space-time-stack/kp_space_time_stack.so"
 export CUDA_LAUNCH_BLOCKING=1
 mpiexec \
     -np 4 \
     "${EXE_SPARTA}" \
-    -k on g 4 -sf kk -in in.cylinder
+    -k on g 4 -sf kk -var ppc 205 -in in.cylinder
