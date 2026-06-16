38e5cba02f110949a7f9e398503bcc3146dc81f5
Anthony Agelastos <amagela@sandia.gov>
2026-06-16 08:54:07 -0600

    Substantial tweaks to the SPARTA bulk launcher script

--- docs/31_sparta/templatedir/sparta_bulk_launcher.sh
+++ docs/31_sparta/templatedir/sparta_bulk_launcher.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
+# salloc -N 1 -n 4 --ntasks-per-node=4 --cpus-per-task=28 --gpus-per-task=1 --gpu-bind=closest -A fy240071 --time=1:00:00 -p short,batch --license=pscratch
+export IS_CTS2=${IS_CTS2:-0}
+export IS_CTS2GPU=${IS_CTS2GPU:-1}
+export IS_ELCAP=${IS_ELCAP:-0}
 
-for m_ppc in ` seq 126 145 ` ; do
-    SPARTA_NSCALE=1 SPARTA_PPC=${m_ppc} SPARTA_IS_KOKKOS_TOOLS="yes" APP_REPEAT=1 \
-        sbatch \
-            --nodes=1 \
-            --account=fy140252 \
-            --job-name=fcr30sparta \
-            --time=0:14:59 \
-            --partition=batch,short \
-            sparta_sbatch_cts2.sh
+for m_ppc in ` seq 45 10 205 ` ; do
+    if test ${IS_CTS2} -eq 1 ; then
+        SPARTA_NSCALE=1 SPARTA_PPC=${m_ppc} SPARTA_IS_KOKKOS_TOOLS="yes" APP_REPEAT=1 \
+            sbatch \
+                --nodes=1 \
+                --account=fy150069 \
+                --job-name=fcr30sparta \
+                --time=0:14:59 \
+                --partition=batch,short \
+                sparta_sbatch_cts2.sh
+    fi
+
+    if test ${IS_CTS2GPU} -eq 1 ; then
+        SPARTA_NSCALE=1 SPARTA_PPC=${m_ppc} SPARTA_IS_KOKKOS_TOOLS="yes" APP_REPEAT=1 \
+            sbatch \
+                --nodes=1 \
+                --ntasks=4 \
+                --ntasks-per-node=4 \
+                --cpus-per-task=28 \
+                --gpus-per-task=1 \
+                --gpu-bind=closest \
+                --account=fy150069 \
+                --time=0:14:59 \
+                --partition=batch,short \
+                --job-name=fcr30sparta \
+                sparta_sbatch_cts2gpu.sh
+    fi
+
+    if test ${IS_ELCAP} -eq 1 ; then
+        SPARTA_NSCALE=1 SPARTA_PPC=${m_ppc} SPARTA_IS_KOKKOS_TOOLS="yes" APP_REPEAT=1 \
+            flux batch sparta_batch_elcapitan.sh
+    fi
     sleep 0.5
 done
 
