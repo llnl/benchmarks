b5cb45a25d8c06964e2dd058706faeb899ba66b1
Anthony Agelastos <amagela@sandia.gov>
2026-06-16 08:53:22 -0600

    Made tweaks to interactive script for SPARTA on CTS-2+GPU

--- docs/31_sparta/templatedir/sparta_interactive_cts2gpu.sh
+++ docs/31_sparta/templatedir/sparta_interactive_cts2gpu.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# salloc -N 1 -n 4 --ntasks-per-node=4 --cpus-per-task=28 --gpus-per-task=1 --gpu-bind=closest -A fy240071 --time=1:00:00 -p short,batch --license=pscratch
 . sparta_env_cts2gpu.sh
 export EXE_SPARTA="${EXE_SPARTA:-sparta/_build-cts2gpu/src/spa_kokkos_cuda}"
 export KOKKOS_TOOLS_LIBS="/tscratch/amagela/fcr/sparta-final/llnl-benchmarks-sparta/docs/31_sparta/kokkos-tools-cts2gpu/profiling/space-time-stack/kp_space_time_stack.so"
