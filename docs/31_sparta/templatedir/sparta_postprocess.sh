0d76d2d84a4c51fd74755a2254f95b3b14c5957e
Anthony Agelastos <amagela@sandia.gov>
2026-06-23 16:20:31 -0600

    Differentiated options for SPARTA postprocessing with and without memory.

--- docs/31_sparta/templatedir/sparta_postprocess.sh
+++ docs/31_sparta/templatedir/sparta_postprocess.sh
@@ -27,11 +27,18 @@
 #END KOKKOS PROFILING REPORT.
 
 
+export IS_SPARTA_MEMORY=${IS_SPARTA_MEMORY:-0}
+
 file_name="log.sparta"
 file_result="${file_name}.csv"
 file_tmp="${file_result}.tmp"
 
-echo "PPC,NSCALE,ranks,GPUs/node,threads/rank,FOM,Host Mean MaxRSS (KiB),Host Mean Nodal MaxRSS (GiB),Max CUDA (KiB),Max Nodal CUDA (GiB),Max HIP (KiB),Max Nodal HIP (GiB),FILE" > "${file_tmp}"
+if test ${IS_SPARTA_MEMORY} -eq 0 ; then
+    echo "PPC,NSCALE,ranks,GPUs/node,threads/rank,FOM,FILE" > "${file_tmp}"
+else
+    echo "PPC,NSCALE,ranks,GPUs/node,threads/rank,FOM,Host Mean MaxRSS (KiB),Host Mean Nodal MaxRSS (GiB),Max CUDA (KiB),Max Nodal CUDA (GiB),Max HIP (KiB),Max Nodal HIP (GiB),FILE" > "${file_tmp}"
+fi
+
 find . \
      -type f \
      -name "${file_name}" \
@@ -45,16 +52,21 @@ while IFS= read -r -d '' file; do  # <- read each name into $file
     nscale=`grep -oP '^ Weak scaling factor \(nscale\) = \K[+-]?\d+' "${file}" | sed -n '1{p;q;}'`
     fom=`grep -oP '^Performance:.*?[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?.*?\K[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?' "${file}" | sed -n '2{p;q;}'`
 
-    maxrss=`grep -zPo '(?m)^Host process high water mark memory consumption:[ \t]*\d+[ \t]*kB\n[ \t]*Max:[ \t]*\d+,[ \t]*Min:[ \t]*\d+,[ \t]*Ave:[ \t]*\K\d+(?=[ \t]*kB)' "${dir_spartalog}"/output-launcher*.log | tr '\0' '\n'`
-    mem_host_node=`echo "((${maxrss}*(${ranks}/${nscale}))/1024)/1024" | bc -l`
+    if test ${IS_SPARTA_MEMORY} -eq 0 ; then
+        echo "${ppc},${nscale},${ranks},${gpus_per_node},${threads_per_rank},${fom},${file}" >> "${file_tmp}"
+    else
+        maxrss=`grep -zPo '(?m)^Host process high water mark memory consumption:[ \t]*\d+[ \t]*kB\n[ \t]*Max:[ \t]*\d+,[ \t]*Min:[ \t]*\d+,[ \t]*Ave:[ \t]*\K\d+(?=[ \t]*kB)' "${dir_spartalog}"/output-launcher*.log | tr '\0' '\n'`
+        mem_host_node=`echo "((${maxrss}*(${ranks}/${nscale}))/1024)/1024" | bc -l`
 
-    cuda_single=`grep -A2 -m1 '^KOKKOS CUDA SPACE:$' "${dir_spartalog}"/output-launcher*.log | grep -oP '^MAX MEMORY ALLOCATED:[[:space:]]*\K[-+]?[0-9]+(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?(?=[[:space:]]*kB)'`
-    cuda_node=`echo "((${cuda_single}*${gpus_per_node})/1024)/1024" | bc -l`
+        cuda_single=`grep -A2 -m1 '^KOKKOS CUDA SPACE:$' "${dir_spartalog}"/output-launcher*.log | grep -oP '^MAX MEMORY ALLOCATED:[[:space:]]*\K[-+]?[0-9]+(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?(?=[[:space:]]*kB)'`
+        cuda_node=`echo "((${cuda_single}*${gpus_per_node})/1024)/1024" | bc -l`
 
-    hip_single=`grep -A2 -m1 '^KOKKOS HIP SPACE:$' "${dir_spartalog}"/output-launcher*.log | grep -oP '^MAX MEMORY ALLOCATED:[[:space:]]*\K[-+]?[0-9]+(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?(?=[[:space:]]*kB)'`
-    hip_node=`echo "((${hip_single}*4)/1024)/1024" | bc -l`
+        hip_single=`grep -A2 -m1 '^KOKKOS HIP SPACE:$' "${dir_spartalog}"/output-launcher*.log | grep -oP '^MAX MEMORY ALLOCATED:[[:space:]]*\K[-+]?[0-9]+(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?(?=[[:space:]]*kB)'`
+        hip_node=`echo "((${hip_single}*4)/1024)/1024" | bc -l`
 
-    echo "${ppc},${nscale},${ranks},${gpus_per_node},${threads_per_rank},${fom},${maxrss},${mem_host_node},${cuda_single},${cuda_node},${hip_single},${hip_node},${file}" >> "${file_tmp}"
+        echo "${ppc},${nscale},${ranks},${gpus_per_node},${threads_per_rank},${fom},${maxrss},${mem_host_node},${cuda_single},${cuda_node},${hip_single},${hip_node},${file}" >> "${file_tmp}"
+    fi
+    
 done
 
 (head -n 1 "${file_tmp}" && tail -n +2 "${file_tmp}" | sort -t, -k1,1n) > "${file_result}"
