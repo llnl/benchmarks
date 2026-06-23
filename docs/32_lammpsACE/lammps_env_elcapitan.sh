eee6ef85b371ae14410161fe4ee4afef722dcef9
Anthony Agelastos <amagela@sandia.gov>
2026-06-23 17:24:22 -0600

    Updated LAMMPS environment scripts.

--- docs/32_lammpsACE/lammps_env_elcapitan.sh
+++ docs/32_lammpsACE/lammps_env_elcapitan.sh
@@ -2,7 +2,8 @@
 
 module load craype-accel-amd-gfx942
 module load PrgEnv-cray
-module load rocm/6.2.1
+module load rocm/6.4.3
+module load cray-mpich/9.0.1
 module load python
 
 export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
@@ -11,7 +12,7 @@ export MPICH_GPU_SUPPORT_ENABLED=1
 export MPICH_OFI_NIC_POLICY=GPU
 
 ### FIXME ### Need a system wide install of libfabric from SHS 11 (or newer)
-export LD_LIBRARY_PATH=/usr/workspace/wsb/accept/packages-2024/SHS11_lib:${LD_LIBRARY_PATH}
+# export LD_LIBRARY_PATH=/usr/workspace/wsb/accept/packages-2024/SHS11_lib:${LD_LIBRARY_PATH}
 
 export HIP_PATH=`hipconfig -p`
 export LD_LIBRARY_PATH=${HIP_PATH}/lib:${LD_LIBRARY_PATH}
@@ -23,6 +24,6 @@ export FI_HMEM="rocr"
 export HUGETLB_MORECORE=yes
 
 # restrict libhugetlbfs to be enabled for these executables only:
-export HUGETLB_RESTRICT_EXE="defrag:lmp"
+# export HUGETLB_RESTRICT_EXE="defrag:lmp"
 
 export HSA_XNACK=1
