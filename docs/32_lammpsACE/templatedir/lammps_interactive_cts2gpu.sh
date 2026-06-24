05d23be419792fa9a63104afe7eef4ee37e3895d
Anthony Agelastos <amagela@sandia.gov>
2026-06-23 20:52:23 -0600

    Updated LAMMPS interactive script for CTS-2+GPU to leverage Kokkos Tools.

--- docs/32_lammpsACE/templatedir/lammps_interactive_cts2gpu.sh
+++ docs/32_lammpsACE/templatedir/lammps_interactive_cts2gpu.sh
@@ -2,12 +2,12 @@
 # salloc -N 1 -n 4 --ntasks-per-node=4 --cpus-per-task=28 --gpus-per-task=1 --gpu-bind=closest -A fy240071 --time=1:00:00 -p short,batch --license=pscratch
 . lammps_env_cts2gpu.sh
 export EXE_LAMMPS="${EXE_LAMMPS:-lammps/_build-cts2gpu/lmp}"
-# export KOKKOS_TOOLS_LIBS="/tscratch/amagela/fcr/sparta-final/llnl-benchmarks-sparta/docs/31_sparta/kokkos-tools-cts2gpu/profiling/space-time-stack/kp_space_time_stack.so"
-# export CUDA_LAUNCH_BLOCKING=1
+export KOKKOS_TOOLS_LIBS="/rlfs01/amagela/fcr/lammps/llnl-benchmarks/docs/32_lammpsACE/kokkos-tools-cts2gpu/profiling/space-time-stack/kp_space_time_stack.so"
 mpiexec \
-    -np 8 \
+    -np 4 \
     "${EXE_LAMMPS}" \
-    -k on g 4 -sf kk -pk kokkos newton on neigh half \
-    -var Lprime 64 \
-    -var nscale 2 \
-    -in in.pace.product
+        -k on g 4 -sf kk -pk kokkos newton on neigh half \
+        -var nscale 1 \
+        -var pacechunksize 20000 \
+        -var Lprime 64 \
+        -in in.pace.product
