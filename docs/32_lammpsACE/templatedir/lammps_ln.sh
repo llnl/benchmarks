2e9f8056cfbf05e638b3932d638d0cef964bfba5
Anthony Agelastos <amagela@sandia.gov>
2026-06-23 18:01:42 -0600

    Updated LAMMPS symlinker

--- docs/32_lammpsACE/templatedir/lammps_ln.sh
+++ docs/32_lammpsACE/templatedir/lammps_ln.sh
@@ -2,9 +2,7 @@
 
 set -v
 
-test ! -e lammps_env_elcapitan.sh        && ln -s ../lammps_env_elcapitan.sh
-test ! -e kokkos_tools_env_elcapitan.sh  && ln -s ../kokkos_tools_env_elcapitan.sh
 test ! -e lammps                         && ln -s ../lammps
-test ! -e Cu-PBE-core-rep.ace            && ln -s ../lammps/examples/PACKAGES/pace/Cu-PBE-core-rep.ace
+test ! -e Cu-PBE-core-rep.ace            && ln -s ../lammps/potentials/Cu-PBE-core-rep.ace
 
 exit 0
